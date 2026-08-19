15e8bce5fedb429698be98e9cbb60375cd606b08
Mauro Fini <mauro.fini.it@gmail.com>
2026-08-19 14:02:39 +0200

    Aggiornato script backup_db.ps1

--- backup_db.ps1
+++ backup_db.ps1
@@ -14,3 +14,15 @@
 # git diff --name-only origin/main
 # Per visualizzare esattamente cosa è cambiato all'interno dei file:
 # git diff origin/main
+
+
+# Esegui questi comandi per tracciarli, fare il commit e inviarli al repository remoto:
+# git add .
+# git commit -m "Aggiunti script PowerShell per backup DB e verifica trascrizioni"
+# git push
+
+# Se preferisci che questi script rimangano solo sul tuo computer locale, aggiungili al file .gitignore:
+# Add-Content .gitignore "`nbackup_db.ps1`nverifica_trascrizioni.ps1"
+# git add .gitignore
+# git commit -m "Aggiornato .gitignore per ignorare gli script locali"
+# git push
